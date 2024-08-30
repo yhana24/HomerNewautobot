7ee7511b565c3cda5526a099fddcdac8dd3c7a0e
zeka321 <jkingzeka@gmail.com>
2024-08-31 00:44:32 +0800

    Update sendnoti.js

--- commands/sendnoti.js
+++ commands/sendnoti.js
@@ -25,7 +25,7 @@ module.exports = {
       if (thread.isGroup && thread.threadID !== event.threadID) {
         try {
           await api.sendMessage(
-            `➜ 𝗡𝗢𝗧𝗜𝗙𝗜𝗖𝗔𝗧𝗜𝗢𝗡 𝗙𝗥𝗢𝗠 𝗔𝗗𝗠𝗜𝗡\n\n➜ ${customMessage}\n\nAutoBotDev: https://www.facebook.com/profile.php?id=100088690249020`,
+            `➜ 𝗡𝗢𝗧𝗜𝗙𝗜𝗖𝗔𝗧𝗜𝗢𝗡 𝗙𝗥𝗢𝗠 𝗔𝗗𝗠𝗜𝗡\n\n➜ ${customMessage}\n\nAutoBotDev: https://www.facebook.com/profile.php?id=61564074353674`,
             thread.threadID
           );
           sentCount++;
